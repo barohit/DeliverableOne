b75f59c9f5e9af2d5c2dff81025a52e918bf0479
Rohit <rohit.r.baddam@gmail.com>
2019-03-24 20:29:35 -0400

    Second update

--- src/deliverableOne/WhatToEat.java
+++ src/deliverableOne/WhatToEat.java
@@ -17,8 +17,8 @@ public class WhatToEat {
 		System.out.println("What is the event type? You can enter 'casual', 'semi-formal', or 'formal'.");
 		eventType += scnr.nextLine();
 
-		// While loop ensures that the users enters an allowed event type.
-		while ((!(eventType.equals("casual")) && !(eventType.equals("semi-formal")) && !(eventType.equals("formal")))) {
+		// While loop ensures that the user enters an allowed event type.
+		while (!(eventType.equals("casual") || eventType.equals("semi-formal") || eventType.equals("formal"))) {
 
 			System.out.println("Error, invalid entry. Please try again:");
 			eventType = scnr.next();
@@ -38,24 +38,30 @@ public class WhatToEat {
 
 		scnr.close();
 
-		if (eventType.equals("casual")) {
+		switch (eventType) {
 
+		case "casual":
 			mealSuggestion = "sandwiches";
-		}
+			break;
 
-		else if (eventType.equals("semi-formal")) {
-
+		case "semi-formal":
 			mealSuggestion = "fried chicken";
+			break;
+
+		case "formal":
+			mealSuggestion = "checken parmesan";
+			break;
+
+		default:
+			mealSuggestion = "huh?";
+			break;
+
 		}
-
-		else if (eventType.equals("formal")) {
-
-			mealSuggestion = "chicken parmesean";
-		}
-
 		// mealSuggestion assignment ends and preparationSuggestion assignment begins.
 
-		if (partySize == 1) {
+		if (partySize == 1)
+
+		{
 
 			preparationSuggestion = "in the microsave";
 		}
@@ -72,17 +78,13 @@ public class WhatToEat {
 				if (partySize >= 13) {
 
 					preparationSuggestion = "by a caterer";
-
 				}
 			}
-
 		}
 
 		result = "Since you are hosting a " + eventType + " event for " + partySize + " participants, you should serve "
 				+ mealSuggestion + " prepared " + preparationSuggestion + ".";
 
 		System.out.println(result);
-
 	}
-
 }
